a68153aed3d1125e36f9191f354fc88d288bf191
Amaury Loubry <aloubry@c2r3s7.42lausanne.ch>
2025-01-30 16:45:06 +0100

    start cpp00

---
+++ cpp00/ex01/mgaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if(argc != 1)
+	{
+		argv++;
+		while(*argv)
+		{
+			while(**argv)
+			{
+				std::cout << toupper(**argv);
+				(*argv)++;
+			}
+			argv++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+}
