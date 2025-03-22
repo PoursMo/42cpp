1fca3d46961b0416ba1c6fb2503fb87cfad27972
Amaury <loubry.amaury@gmail.com>
2025-03-22 12:05:45 +0100

    add variant to cpp08 ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -114,4 +114,19 @@ public:
 	}
 };
 
+// template <typename T>
+// class MutantStack : public std::stack<T>
+// {
+// public:
+// 	typedef typename std::stack<T>::container_type::iterator iterator;
+// 	iterator begin()
+// 	{
+// 		return this->c.begin();
+// 	}
+// 	iterator end()
+// 	{
+// 		return this->c.end();
+// 	}
+// };
+
 #endif
